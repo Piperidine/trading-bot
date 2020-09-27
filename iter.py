e452a3d881d48aa5f3b72882425cbc35de564f5b
Anvay Varerkar <anvay.varerkar@gmail.com>
2020-09-27 14:50:59 +0530

    Changed Function

--- iter.py
+++ iter.py
@@ -1,3 +1,31 @@
+def notif():
+    import requests
+    from datetime import datetime
+    import time
+
+    url = 'https://fcm.googleapis.com/fcm/send'
+
+    headers = {
+        "Content-Type" : "application/json",
+        "Authorization" : "key=AAAA6s6LYFE:APA91bFNRwKoWzgY56PnPGbmVpLmMboHh1hTm8GtSM1tIIFaXPv3x0hVVl8PbH5Z5nmfkIfiJ7oi1yf3SdBVUC9VZVqeNJ4P9s2yrp0dN5HvJKkbohP3kX_mcxsHyLbjM2uj1WDVq0n4"      }
+
+    myobj = {
+        "to": "/topics/all",
+        "notification": {
+        "title": "Training Completed",
+        "body": "Training on AWS Completed at {}:{} Hours".format(datetime.now().hour,datetime.now().minute),
+        "mutable_content": True,
+        "sound": "Tri-tone"
+        },
+
+    "data": {
+        "url": "<url of media image>",
+        "dl": "<deeplink action on tap of notification>"
+        }
+    }
+
+    x = requests.post(url, headers = headers, json = myobj)
+
 import train
 ticker_list = ['ADANIPORTS', 'ASIANPAINT', 'AXISBANK', 'BAJAJ-AUTO', 'BAJAJFINSV', 'BPCL', 'BHARTIARTL', 'INFRATEL', 'BRITANNIA', 'CIPLA', 'COALINDIA', 'DRREDDY', 'EICHERMOT', 'GAIL', 'GRASIM', 'HCLTECH', 'HDFCBANK', ]
 # ticker_list = ['BJ']
@@ -13,29 +41,4 @@ for ticker in ticker_list:
     debug = True
     train.main(train_stock, val_stock, window_size, batch_size, ep_count, strategy=strategy, model_name=model_name, pretrained=pretrained, debug=debug)
 
-import requests
-from datetime import datetime
-import time
-
-url = 'https://fcm.googleapis.com/fcm/send'
-
-headers = {
-    "Content-Type" : "application/json",
-    "Authorization" : "key=AAAA6s6LYFE:APA91bFNRwKoWzgY56PnPGbmVpLmMboHh1hTm8GtSM1tIIFaXPv3x0hVVl8PbH5Z5nmfkIfiJ7oi1yf3SdBVUC9VZVqeNJ4P9s2yrp0dN5HvJKkbohP3kX_mcxsHyLbjM2uj1WDVq0n4"      }
-
-myobj = {
-    "to": "/topics/all",
-    "notification": {
-      "title": "Training Completed",
-      "body": "Training on AWS Completed at {}:{} Hours".format(datetime.now().hour,datetime.now().minute),
-      "mutable_content": True,
-      "sound": "Tri-tone"
-      },
-
-   "data": {
-    "url": "<url of media image>",
-    "dl": "<deeplink action on tap of notification>"
-      }
-}
-
-x = requests.post(url, headers = headers, json = myobj)
+notif()
